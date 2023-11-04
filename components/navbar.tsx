e2a4c2ea123c3878e2876476649bb65a861d92f3
Sabine Sun <sabine.sun@hotmail.com>
2023-11-04 13:59:31 +0100

    feat(frontend): add navbar ui

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,3 +1,35 @@
+import { Typography } from "@/components/ui/typography";
+import { ShoppingBasket } from "lucide-react";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+
 export const Navbar = () => {
-  return <header className="bg-blue-500 p-4">Header</header>;
+  return (
+    <header className="bg-secondBackground flex flex-row items-center px-7 py-2.5">
+      <Typography variant="h1" className="w-full text-center">
+        The shopping store
+      </Typography>
+      <Sheet>
+        <SheetTrigger className="flex items-center">
+          <ShoppingBasket />
+        </SheetTrigger>
+        <SheetContent>
+          <SheetHeader className="h-full">
+            <SheetTitle className="border-accent flex justify-center border-b-2 pb-4">
+              Panier
+            </SheetTitle>
+            <SheetDescription className="flex h-full items-center justify-center">
+              Votre panier est vide.
+            </SheetDescription>
+          </SheetHeader>
+        </SheetContent>
+      </Sheet>
+    </header>
+  );
 };
